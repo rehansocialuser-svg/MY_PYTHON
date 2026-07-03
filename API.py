ad385cad394cfd761771f65ebf7cf2c7e53484fb
rehansocialuser-svg <rehansocialuser@gmail.com>
2026-07-03 20:40:27 +0530

    added bulk post method

--- API.py
+++ API.py
@@ -1,1 +1,22 @@
-print("helloworld")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class StudentModel(BaseModel):
+    name: str
+    age: int
+
+students_db = []
+
+@app.post("/students")
+def create_student(student_data: StudentModel):
+    students_db.append(student_data.dict())
+    return {
+        "msg": "student created successfully",
+        "student": student_data
+    }
+
+@app.get("/student-data")
+def get_all_students():
+    return students_db
